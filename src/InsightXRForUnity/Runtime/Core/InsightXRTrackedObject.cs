f3bfc8229c8a8314392015793e0777013e5cbdaf
Kraaven <dhruv@getinsightxr.com>
2024-04-22 18:47:52 +0530

    Updated the SDK
    -Fixed the Car Demo Scene
    -Implemented Local Axis transforms data collection
    -Configured the UltimateXR scene

--- src/InsightXRForUnity/Runtime/Core/InsightXRTrackedObject.cs
+++ src/InsightXRForUnity/Runtime/Core/InsightXRTrackedObject.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using InsightXR.Channels;
 using InsightXR.Network;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace InsightXR.Core
@@ -15,23 +16,35 @@ namespace InsightXR.Core
         [Header ("Broadcasting to")]
         [SerializeField] private ComponentDataDistributionChannel DistributionChannel;
         
+        
+        
 
         private void OnEnable() => DataCollectorWebMode.DistributionRequestEvent += MoveObject;
 
         private void OnDisable() => DataCollectorWebMode.DistributionRequestEvent -= MoveObject;
-
-        private void Start()
-        {
-        }
+        
 
         private void FixedUpdate() {
             
-            DistributionChannel.RaiseEvent(name, new(transform.position, transform.rotation));
+            if (transform.parent != null)
+            {
+                DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,transform.parent.name)); 
+            }
+            else
+            {
+                DistributionChannel.RaiseEvent(name, new(transform.localPosition, transform.localRotation,"World"));
+            }
+            
         } 
 
-        private void MoveObject(string name, ObjectData setToPoint){
-            if(gameObject.name.Equals(name)) transform.SetPositionAndRotation(setToPoint.GetPosition(), setToPoint.GetRotation());
-            
+        private void MoveObject(string name, ObjectData setToPoint)
+        {
+            if (gameObject.name.Equals(name))
+            {
+                transform.parent = setToPoint.ParentObject == "World" ? null : GameObject.Find(setToPoint.ParentObject).transform;
+                transform.SetLocalPositionAndRotation(setToPoint.GetPosition(), setToPoint.GetRotation());
+            }
+
         }
     }
 }
